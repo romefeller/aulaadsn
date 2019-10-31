32b081b819e77db4a55ccd3adf4e8e711b569544
Ubuntu <ubuntu@ip-172-31-29-143.ec2.internal>
2019-10-31 00:10:03 +0000

    MENU

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -10,6 +10,19 @@ import Import
 --import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
+getPage2R :: Handler Html 
+getPage2R = do 
+    defaultLayout $ do 
+        $(whamletFile "templates/page2.hamlet")
+
+getPage1R :: Handler Html 
+getPage1R = do 
+    defaultLayout $ do 
+        addStylesheet (StaticR css_bootstrap_css)
+        toWidgetHead $(juliusFile "templates/page1.julius")
+        toWidgetHead $(luciusFile "templates/page1.lucius")
+        $(whamletFile "templates/page1.hamlet")
+
 getHomeR :: Handler Html
 getHomeR = do 
     defaultLayout $ do 
@@ -26,11 +39,25 @@ getHomeR = do
             h1 {
                 color : red;
             }
+            
+            ul {
+                display: inline;
+                list-style: none;
+            }
         |]
         [whamlet|
             <div>
                 <h1>
                     OLA MUNDO
+            
+            <ul>
+                <li>
+                    <a href=@{Page1R}>
+                        Pagina 1
+                
+                <li>
+                    <a href=@{Page2R}>
+                        Pagina 2
             
             <img src=@{StaticR citeg_jpg}>
             

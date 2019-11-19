dc7ee8409b4cd5f97b5c70008c9cb01498a23ecb
Ubuntu <ubuntu@ip-172-31-29-143.ec2.internal>
2019-11-19 22:57:11 +0000

    Ator-serie

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -29,6 +29,8 @@ instance Yesod App where
     isAuthorized HomeR _ = return Authorized
     isAuthorized EntrarR _ = return Authorized
     isAuthorized UsuarioR _ = return Authorized 
+    isAuthorized AtorR _ = return Authorized
+    isAuthorized SerieR _ = return Authorized
     isAuthorized (StaticR _) _ = return Authorized
     isAuthorized AdminR _ = isAdmin
     isAuthorized _ _ = isUsuario

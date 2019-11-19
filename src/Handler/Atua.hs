4c9dc57c01173b4383fdffe9485cb3969cbce5f9
Ubuntu <ubuntu@ip-172-31-29-143.ec2.internal>
2019-11-19 23:42:44 +0000

    JOIN

--- src/Handler/Atua.hs
+++ src/Handler/Atua.hs
@@ -68,7 +68,7 @@ getElencoR serieid = do
           \ INNER JOIN ator ON atua.atorid = ator.id \
           \ WHERE serie.id = ?"
     serie <- runDB $ get404 serieid
-    atores <- runDB $ rawSql sql [toPersistValue serieid]
+    atores <- runDB $ rawSql sql [toPersistValue serieid] :: Handler [(Entity Serie,Entity Atua,Entity Ator)]
     defaultLayout $ do 
         [whamlet|
             <h1>

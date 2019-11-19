93cf28e9e98062039eea26598b74a8adac11225f
Ubuntu <ubuntu@ip-172-31-29-143.ec2.internal>
2019-11-19 23:44:00 +0000

    JOIN

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -32,6 +32,7 @@ instance Yesod App where
     isAuthorized AtorR _ = return Authorized
     isAuthorized SerieR _ = return Authorized
     isAuthorized AtuaR _ = return Authorized
+    isAuthorized (ElencoR _) _ = return Authorized
     isAuthorized (StaticR _) _ = return Authorized
     isAuthorized AdminR _ = isAdmin
     isAuthorized _ _ = isUsuario
